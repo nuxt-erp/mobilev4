6403abdb8efdcbac8f36b37c3efedd06bd10d802
Matthew Wierzbicki <mateuszwierzbicki97@gmail.com>
2020-12-31 10:49:27 -0500

    Home page navigation

--- app/src/main/java/com/github/htdangkhoa/nexterp/ui/main/fragments/home/index/HomeIndexFragment.kt
+++ app/src/main/java/com/github/htdangkhoa/nexterp/ui/main/fragments/home/index/HomeIndexFragment.kt
@@ -5,9 +5,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.github.htdangkhoa.nexterp.R
 import com.github.htdangkhoa.nexterp.base.BaseFragment
 import com.github.htdangkhoa.nexterp.ui.main.fragments.home.HomeViewModel
+import com.github.htdangkhoa.nexterp.ui.main.fragments.stockcount.new.StockCountNewFragmentDirections
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
 class HomeIndexFragment() : BaseFragment<HomeViewModel>(
@@ -16,14 +19,23 @@ class HomeIndexFragment() : BaseFragment<HomeViewModel>(
     override val layoutResID: Int
         get() = R.layout.fragment_home
 
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        return inflater.inflate(R.layout.fragment_home, container, false)
-//    }
-
     override fun render(view: View, savedInstanceState: Bundle?) {
+        btnListReceiving.setOnClickListener {
+            val action = HomeIndexFragmentDirections.actionNavHomeToNavReceiving()
+            view.findNavController().navigate(action)
+        }
+        btnNewReceiving.setOnClickListener {
+            val action = HomeIndexFragmentDirections.actionNavHomeToReceivingNewFragment()
+            view.findNavController().navigate(action)
+        }
+        btnListStockCount.setOnClickListener {
+            val action = HomeIndexFragmentDirections.actionNavHomeToNavStockCount()
+            view.findNavController().navigate(action)
+        }
+        btnNewStockCount.setOnClickListener {
+            val action = HomeIndexFragmentDirections.actionNavHomeToStockCountNewFragment()
+            view.findNavController().navigate(action)
+        }
 
     }
 }
--- app/src/main/java/com/github/htdangkhoa/nexterp/ui/main/fragments/receiving/new/ReceivingNewFragment.kt
+++ app/src/main/java/com/github/htdangkhoa/nexterp/ui/main/fragments/receiving/new/ReceivingNewFragment.kt
@@ -1,4 +1,47 @@
+
 package com.github.htdangkhoa.nexterp.ui.main.fragments.receiving.new
 
-class ReceivingNewFragment {
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.navigation.findNavController
+import com.github.htdangkhoa.nexterp.R
+import com.github.htdangkhoa.nexterp.base.BaseFragment
+import com.github.htdangkhoa.nexterp.data.remote.bin.BinResponse
+import com.github.htdangkhoa.nexterp.data.remote.brand.BrandResponse
+import com.github.htdangkhoa.nexterp.data.remote.category.CategoryResponse
+import com.github.htdangkhoa.nexterp.data.remote.stockcount.stockcount.StockCountResponse
+import com.github.htdangkhoa.nexterp.data.remote.stocklocator.StockLocatorResponse
+import com.github.htdangkhoa.nexterp.data.remote.tag.TagResponse
+import com.github.htdangkhoa.nexterp.resource.ObserverResource
+import com.github.htdangkhoa.nexterp.ui.adapters.CheckableSpinnerAdapter
+import com.github.htdangkhoa.nexterp.ui.adapters.CheckableSpinnerAdapter.SpinnerItem
+import com.github.htdangkhoa.nexterp.ui.main.fragments.receiving.ReceivingViewModel
+import com.github.htdangkhoa.nexterp.ui.main.fragments.stockcount.StockCountViewModel
+import com.github.htdangkhoa.nexterp.ui.main.fragments.stockcount.list.StockCountListFragmentDirections
+import com.pawegio.kandroid.hide
+import com.pawegio.kandroid.show
+import com.pawegio.kandroid.toast
+import kotlinx.android.synthetic.main.fragment_stockcount_list.progressCircular
+import kotlinx.android.synthetic.main.fragment_stockcount_new.*
+
+
+class ReceivingNewFragment() : BaseFragment<ReceivingViewModel>(
+    ReceivingViewModel::class
+) {
+
+    override val layoutResID: Int
+        get() = R.layout.fragment_receiving_new
+
+    override fun render(view: View, savedInstanceState: Bundle?) {
+
+        btnSaveFilters.setOnClickListener {
+            val myName = nameInput.text.toString()
+            if(myName.isNotEmpty()) {
+//                viewModel.newStockCount(createMap(), myName)
+            } else {
+                showDialog("Error", "You must enter a name for the Stock Count.")
+            }
+        }
+    }
 }
